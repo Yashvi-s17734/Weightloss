eaeb1904d30e3bf959ac98d4bf6653589dcecc65
Yashvi-s17734 <yashvipshah80@gmail.com>
2026-01-21 21:48:02 +0530

    use render backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:5000/api";
-
+const API_URL = "https://weightloss-oj47.onrender.com/api";
 export const signup = async (data) => {
   const res = await fetch(`${API_URL}/auth/signup`, {
     method: "POST",
